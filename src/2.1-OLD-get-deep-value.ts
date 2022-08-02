58f467873d367b2a0a9a5e5321129e18efe4a142
zBiggui <bigguisenna@gmail.com>
2022-08-02 19:49:33 -0300

    feat: add implementation for getting a deep value inside an object

---
+++ src/2.1-OLD-get-deep-value.ts
@@ -0,0 +1,22 @@
+export const getDeepValue = (
+    obj: any,
+    firstKey: string,
+    secondKey: string
+) => {
+    return obj[firstKey][secondKey]
+}
+
+const obj = {
+    foo: {
+        a: true,
+        b: 2
+    },
+    bar: {
+        c: "12",
+        d: 18,
+    },
+};
+
+const value = getDeepValue(obj, "foo", "b");
+
+typeof value === 'number';
